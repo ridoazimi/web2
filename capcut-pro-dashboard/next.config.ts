ea3dd5b08eb21a32034ccd47faaf99e40e16c4ff
GreenAppl3 <michaelmukti23@gmail.com>
2026-07-06 22:48:45 +0700

    update size limit video

--- capcut-pro-dashboard/next.config.ts
+++ capcut-pro-dashboard/next.config.ts
@@ -20,7 +20,7 @@ const nextConfig: NextConfig = {
   // Increase body size limit for Server Actions to handle larger file uploads (5MB)
   experimental: {
     serverActions: {
-      bodySizeLimit: '5mb',
+      bodySizeLimit: '50mb',
     },
   },
 };
